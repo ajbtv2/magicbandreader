6de5dde38e5fe412b3b0070c3e192853fbeb4962
Andrew <drewjbrandt@gmail.com>
2020-08-05 23:44:31 -0500

    Update checkupdates.py

--- checkupdates.py
+++ checkupdates.py
@@ -35,7 +35,7 @@ def ProcessFetch(char, stdin):
 
 if __name__ == "__main__":
     checkTimeSec = 60
-    gitDir = "/magicbandreader"
+    gitDir = "/home/pi/magicbandreader/"
     while True:
         print("*********** Checking for code update **************")                                                     
     
