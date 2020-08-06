e4c166613042359397aa7105fff769df33fb1675
Andrew <drewjbrandt@gmail.com>
2020-08-05 23:48:59 -0500

    Update checkupdates.py

--- checkupdates.py
+++ checkupdates.py
@@ -44,5 +44,5 @@ if __name__ == "__main__":
             resetCheck = git("--git-dir=" + gitDir + ".git/", "--work-tree=" + gitDir, "reset", "--hard", "origin/master")
             print(str(resetCheck)) 
                                                                                                                                                                 
-        print("Check complete. Waiting for " + str(checkTimeSec) + "seconds until next check...", True)
+        print("Check complete. Waiting for " + str(checkTimeSec) + "seconds until next check...", False)
         time.sleep(checkTimeSec)
